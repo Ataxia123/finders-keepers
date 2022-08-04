ba008fc3343f7c4cd7e094a64a07cf36cf85beb0
Ataxia123 <vp.ca94@gmail.com>
2022-08-04 11:13:36 -0500

    showing aggregate stats

--- packages/react-app/src/views/Home.jsx
+++ packages/react-app/src/views/Home.jsx
@@ -19,128 +19,50 @@ function Home({ yourLocalBalance, readContracts, zdk, address }) {
 
   const yourNFT = zdk.collection({ address: BAYC });
 
-  const [PromiseResult, SetPromiseResult] = React.useState(null);
+  const [udscVol, SetUdscVol] = React.useState(null);
+  const [nftCount, SetnftCount] = React.useState(null);
+  const [chainTokenPrice, SetchainTokenPrice] = React.useState(null);
+  const [ownerCount, SetownerCount] = React.useState(null);
 
   const getStats = () => {
     zdk
       .collectionStatsAggregate({ collectionAddress: BAYC })
       .then(res => {
         console.log(res);
-        SetPromiseResult(res);
-        console.log(PromiseResult);
+        SetUdscVol(res.aggregateStat.salesVolume.usdcPrice);
+        SetnftCount(res.aggregateStat.nftCount);
+        SetchainTokenPrice(res.aggregateStat.salesVolume.chainTokenPrice);
+        SetownerCount(res.aggregateStat.ownerCount);
       })
       .catch(err => {
         console.log(err);
       });
+    console.log("yourNFT", yourNFT);
   };
-
-  console.log("yourNFT", yourNFT);
-  const baycVolume = PromiseResult.aggregateStat.salesVolume.usdcPrice;
-
 
   return (
     <div>
+      <div>ZORA API SHOWCASE</div>
+      <Button onClick={getStats}>Refresh </Button>
       <div style={{ margin: 32 }}>
         <span style={{ marginRight: 8 }}>📝</span>
-        <div>ZORA API SHOWCASE</div>
-        Bayc Historic Volume Is:
+        BAYC is held by:
         <span
           className="highlight"
           style={{ marginLeft: 4, /* backgroundColor: "#f9f9f9", */ padding: 4, borderRadius: 4, fontWeight: "bolder" }}
         >
-          {baycVolume} USDC<Button onClick={getStats}>Refresh </Button>
+          {udscVol} accounts that own {nftCount}Mokees
         </span>
       </div>
       <div style={{ margin: 32 }}>
-        <span style={{ marginRight: 8 }}>✏️</span>
-        Edit your smart contract{" "}
+        <span style={{ marginRight: 8 }}>📝</span>
+        BAYC is held by:
         <span
           className="highlight"
           style={{ marginLeft: 4, /* backgroundColor: "#f9f9f9", */ padding: 4, borderRadius: 4, fontWeight: "bolder" }}
         >
-          YourContract.sol
-        </span>{" "}
-        in{" "}
-        <span
-          className="highlight"
-          style={{ marginLeft: 4, /* backgroundColor: "#f9f9f9", */ padding: 4, borderRadius: 4, fontWeight: "bolder" }}
-        >
-          packages/hardhat/contracts
+          {chainTokenPrice} accounts that own {ownerCount}Mokees
         </span>
-      </div>
-      {!purpose ? (
-        <div style={{ margin: 32 }}>
-          <span style={{ marginRight: 8 }}>👷‍♀️</span>
-          You haven't deployed your contract yet, run
-          <span
-            className="highlight"
-            style={{
-              marginLeft: 4,
-              /* backgroundColor: "#f9f9f9", */ padding: 4,
-              borderRadius: 4,
-              fontWeight: "bolder",
-            }}
-          >
-            yarn chain
-          </span>{" "}
-          and{" "}
-          <span
-            className="highlight"
-            style={{
-              marginLeft: 4,
-              /* backgroundColor: "#f9f9f9", */ padding: 4,
-              borderRadius: 4,
-              fontWeight: "bolder",
-            }}
-          >
-            yarn deploy
-          </span>{" "}
-          to deploy your first contract!
-        </div>
-      ) : (
-        <div style={{ margin: 32 }}>
-          <span style={{ marginRight: 8 }}>🤓</span>
-          The "purpose" variable from your contract is{" "}
-          <span
-            className="highlight"
-            style={{
-              marginLeft: 4,
-              /* backgroundColor: "#f9f9f9", */ padding: 4,
-              borderRadius: 4,
-              fontWeight: "bolder",
-            }}
-          >
-            {purpose}
-          </span>
-        </div>
-      )}
-
-      <div style={{ margin: 32 }}>
-        <span style={{ marginRight: 8 }}>🤖</span>
-        An example prop of your balance{" "}
-        <span style={{ fontWeight: "bold", color: "green" }}>({ethers.utils.formatEther(yourLocalBalance)})</span> was
-        passed into the
-        <span
-          className="highlight"
-          style={{ marginLeft: 4, /* backgroundColor: "#f9f9f9", */ padding: 4, borderRadius: 4, fontWeight: "bolder" }}
-        >
-          Home.jsx
-        </span>{" "}
-        component from
-        <span
-          className="highlight"
-          style={{ marginLeft: 4, /* backgroundColor: "#f9f9f9", */ padding: 4, borderRadius: 4, fontWeight: "bolder" }}
-        >
-          App.jsx
-        </span>
-      </div>
-      <div style={{ margin: 32 }}>
-        <span style={{ marginRight: 8 }}>💭</span>
-        Check out the <Link to="/hints">"Hints"</Link> tab for more tips.
-      </div>
-      <div style={{ margin: 32 }}>
-        <span style={{ marginRight: 8 }}>🛠</span>
-        Tinker with your smart contract using the <Link to="/debug">"Debug Contract"</Link> tab.
       </div>
     </div>
   );
