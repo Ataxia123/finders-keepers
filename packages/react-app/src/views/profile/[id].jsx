3d4c1f19093e4aa52c8acd926e2fd28fa9a75c0b
Ataxia123 <vp.ca94@gmail.com>
2022-08-06 09:54:08 -0500

    fixed logic for profile pages

--- packages/react-app/src/views/profile/[id].jsx
+++ packages/react-app/src/views/profile/[id].jsx
@@ -1,24 +1,20 @@
 import { gql, useQuery } from "@apollo/client";
 import React from "react";
+import { GET_PROFILES } from "../../hooks/api";
 
-const GET_CURRENT_USER = gql`
-  query Profile($id: String!) {
-    profile(id: $id) {
-      id
-      name
-    }
-  }
-`;
-
-function Profile( props ) {
+function Profile(props) {
   // get ID from props
-  let postId = props.id ? props.id : props.match.params.id;
+  let postId = props.match.params.id;
   console.log(props.match.params.id);
   //Get single profile Query
-  const { loading, error, data } = useQuery(GET_CURRENT_USER, {
-    variables: { id: postId },
+  const { loading, error, data } = useQuery(GET_PROFILES, {
+    variables: {
+      id: postId,
+    },
   });
   console.log("Data", data);
+  if (loading) return "Loading...";
+  if (error) return `Error! ${error.message}`;
   return <div style={{ margin: 100 }}>Stuff goes here{postId}</div>;
 }
 export default Profile;
