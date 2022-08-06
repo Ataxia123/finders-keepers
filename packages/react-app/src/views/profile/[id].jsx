1d054dd99987cc41cb1743d19bdbf14bf23f849d
Ataxia123 <vp.ca94@gmail.com>
2022-08-06 10:16:47 -0500

    added individual profile pictures

--- packages/react-app/src/views/profile/[id].jsx
+++ packages/react-app/src/views/profile/[id].jsx
@@ -15,6 +15,32 @@ function Profile(props) {
   console.log("Data", data);
   if (loading) return "Loading...";
   if (error) return `Error! ${error.message}`;
-  return <div style={{ margin: 100 }}>Stuff goes here{postId}</div>;
+  return (
+    <div style={{ margin: 100 }}>
+      <h1>Stuff goes here</h1>
+      <div>{data.profiles.items[0].handle}</div>
+      {data.profiles.items[0].picture ? (
+        data.profiles.items[0].picture.__typename === "NftImage" ? (
+          <img
+            alt="..."
+            style={{ width: "300px", height: "300px", borderRadius: "10px" }}
+            src={data.profiles.items[0].picture.uri}
+          />
+        ) : (
+          <img
+            alt="..."
+            style={{ width: "300px", height: "300px", borderRadius: "10px" }}
+            src={data.profiles.items[0].picture.original.url}
+          />
+        )
+      ) : (
+        <img
+          alt="..."
+          style={{ width: "300px", height: "300px", borderRadius: "10px" }}
+          src="https://lh3.googleusercontent.com/SnPeYEbN776UygTg05HUfamo4CTSAxMt1cvfsDEDT3NkPmZ5RIEX70B80hUHIqO66LIpepSe8u0yDZEdKZYKHEc2FdL5cLsrVYttZ_Q=w600"
+        />
+      )}
+    </div>
+  );
 }
 export default Profile;
